45b14851375d7425721c27ba751e6a4c13402cc8
Textrix <t3xtrix@gmail.com>
2024-06-26 13:00:29 +0900

    Implement UART output using DMA
    - Enabled UART data transmission via DMA for enhanced performance and efficiency.

--- STM32UTIL-EXAMPLE-NUCLEO-H743ZI2/Core/Src/stm32h7xx_it.c
+++ STM32UTIL-EXAMPLE-NUCLEO-H743ZI2/Core/Src/stm32h7xx_it.c
@@ -55,7 +55,8 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-
+extern DMA_HandleTypeDef hdma_usart3_tx;
+extern UART_HandleTypeDef huart3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -197,6 +198,44 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32h7xx.s).                    */
 /******************************************************************************/
+
+/**
+ * @brief This function handles USART3 global interrupt.
+ */
+void USART3_IRQHandler(void)
+{
+	/* USER CODE BEGIN USART3_IRQn 0 */
+#if STM32UTIL_DEBUG_UART_USE_LL
+	if (LL_USART_IsActiveFlag_TC(STM32UTIL_DEBUG_UART)) {
+		LL_USART_ClearFlag_TC(STM32UTIL_DEBUG_UART);
+		STM32UTIL_DEBUG_UART_TC_isr();
+	}
+#endif
+	/* USER CODE END USART3_IRQn 0 */
+	HAL_UART_IRQHandler(&huart3);
+	/* USER CODE BEGIN USART3_IRQn 1 */
+
+	/* USER CODE END USART3_IRQn 1 */
+}
+
+/**
+ * @brief This function handles DMA1 stream7 global interrupt.
+ */
+void DMA1_Stream7_IRQHandler(void)
+{
+	/* USER CODE BEGIN DMA1_Stream7_IRQn 0 */
+#if STM32UTIL_DEBUG_USE_LL
+	if (STM32UTIL_DEBUG_DMA_IsActiveFlag_TC()) {
+		STM32UTIL_DEBUG_DMA_ClearFlag_TC();
+		STM32UTIL_DEBUG_DMA_TC_isr();
+	}
+#endif
+	/* USER CODE END DMA1_Stream7_IRQn 0 */
+	HAL_DMA_IRQHandler(&hdma_usart3_tx);
+	/* USER CODE BEGIN DMA1_Stream7_IRQn 1 */
+
+	/* USER CODE END DMA1_Stream7_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
